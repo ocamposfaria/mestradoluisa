cda14c47a375fcf635b2f8c92c2ffa42c8e39566
ocamposfaria <ocamposfaria@gmail.com>
2024-05-17 18:43:56 -0300

    First commit

---
+++ config.py
@@ -0,0 +1,100 @@
+from dotenv import load_dotenv
+from typing import Tuple
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import OpenAI
+from pandas import DataFrame
+from langchain.chains.qa_with_sources.vector_db import VectorDBQAWithSourcesChain
+from langchain.chains.question_answering.chain import load_qa_chain
+from langchain.callbacks import get_openai_callback
+
+load_dotenv()
+
+config_params = {
+    "TEXT_CHUNK_SIZE": 1000,
+    "TEXT_CHUNK_OVERLAP": 0,
+    "MODEL_MAX_TOKENS": -1,
+    "MODEL_TEMPERATURE": 0,
+    "CONTEXTO": "Você irá responder as perguntas de um usuário sobre arquivos PDF. Responda objetivamente"
+}
+
+def pdf_text_extraction(pdf: str) -> str:
+    """
+    Extrai texto do arquivo PDF.
+
+    Args:
+        pdf (str): Caminho para o arquivo PDF.
+
+    Returns:
+        str: Texto extraído do PDF.
+    """
+    pdf_reader = PdfReader(pdf)
+    text = ""
+
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    return text
+
+def load_knowledge_base(
+        text: str, 
+        chunk_size: int = config_params["TEXT_CHUNK_SIZE"],
+        chunk_overlap: int = config_params["TEXT_CHUNK_OVERLAP"]
+    ) -> FAISS:
+    """
+    Divide o texto em chunks para inputar no modelo
+    de IA generativa - máximo de 4.096 tokens (aprox. 2.000 palavras) - e
+    carrega no FAISS (vector store).
+
+    Args:
+        text (str): Texto a ser dividido.
+        chunk_size (int, optional): Tamanho dos chunks.
+        chunk_overlap (int, optional): Intervalo entre chunks.
+
+    Returns:
+        FAISS: Vector store com os chunks de texto embutidos.
+    """
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+    return knowledge_base
+
+
+def question_over_vector_database(
+        user_question: str,
+        knowledge_base: FAISS = None, 
+        max_tokens: int = config_params["MODEL_MAX_TOKENS"],
+        temperature: int = config_params["MODEL_TEMPERATURE"],
+        context: int = config_params["CONTEXTO"]
+    ) -> Tuple[str, any]:
+    """
+    Usa uma vector store FAISS para responder uma pergunta de um usuário
+    usando o modelo da OpenAI (ChatGPT 3.5).
+
+    Args:
+        knowledge_base (FAISS): 
+
+    Returns:
+        Tuple: Tupla contando a resposta e o callback de custos.
+    """
+    
+    docs = knowledge_base.similarity_search(user_question)
+
+    llm = OpenAI(max_tokens=max_tokens, temperature=temperature)
+
+    chain = load_qa_chain(llm, chain_type="stuff")
+
+    with get_openai_callback() as cb:
+        response = chain.run(input_documents=docs, context=context, question=user_question)
+    
+    return response, cb
---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd 
+from config import *
+
+ai_answers = []
+st.set_page_config(layout="wide")
+df = pd.read_csv('data.csv', delimiter=';')
+
+col01, col02, col03, col04 = st.columns(4)
+
+# with col01:
+  # st.image('image.png')
+st.header('Proposta de algoritmo para análise de formulários do CADE com LLM e LangChain')
+
+# st.markdown('----')
+col11, col12, col13, col14 = st.columns(4)
+with col11:
+  pdf = st.file_uploader("suba o arquivo PDF aqui", type="pdf", label_visibility='hidden')
+
+col21, col22, col23, col24 = st.columns(4)
+
+with col21:
+  secret = st.text_input('secret')
+  generate = st.button('Gerar respostas do LLM', use_container_width=True)
+
+
+if pdf:
+  text = pdf_text_extraction(pdf)
+  knowledge_base = load_knowledge_base(text)
+
+if secret == 'desbloquear': 
+  if generate:
+    for i in df['Pergunta'].to_list():
+      response, cb = question_over_vector_database(user_question=i, knowledge_base=knowledge_base)
+      ai_answers += [response]
+
+if ai_answers != []:
+  df['Resposta do Large Language Model'] = ai_answers
+
+st.dataframe(df, use_container_width=True, height=650)
