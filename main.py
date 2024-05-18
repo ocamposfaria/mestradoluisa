0b2ad463b9955be96e94bf24d34b9fa187f4dd13
ocamposfaria <ocamposfaria@gmail.com>
2024-05-18 14:28:46 -0300

    Threading para resultados mais rápidos

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd 
+import concurrent.futures
 from config import *
 
-ai_answers = []
 st.set_page_config(layout="wide")
 df = pd.read_csv('data.csv', delimiter=';')
+ai_answers = [None] * len(df['Pergunta'])
 
 col01, col02, col03, col04 = st.columns(4)
 
@@ -12,7 +13,6 @@ with col01:
   st.image('image.png')
 st.header('Proposta de algoritmo para análise de formulários do CADE com LLM e LangChain')
 
-# st.markdown('----')
 col11, col12, col13, col14 = st.columns(4)
 with col11:
   pdf = st.file_uploader("suba o arquivo PDF aqui", type="pdf", label_visibility='hidden')
@@ -33,9 +33,19 @@ if generate:
     st.error('Você precisa subir um PDF antes de fazer isso.')
   else:
     st.success('Extraindo informações do PDF. Aguarde!')
-    for i in df['Pergunta'].to_list():
-      response, cb = question_over_vector_database(user_question=i, knowledge_base=knowledge_base)
-      ai_answers += [response]
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+
+        future_to_index = {
+            executor.submit(lambda idx, i: (idx, question_over_vector_database(user_question=i, knowledge_base=knowledge_base)[0]), idx, i): idx
+            for idx, i in enumerate(df['Pergunta'].to_list())
+        }
+        
+        for future in concurrent.futures.as_completed(future_to_index):
+          idx, response = future.result()
+          ai_answers[idx] = response
+
+
 if ai_answers != []:
   df['Resposta do Large Language Model'] = ai_answers
 
